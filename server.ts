5a3033dc43406a9344c7cb2aa7827ba96ae8827d
shinking02 <k.shin0102@gmail.com>
2023-05-23 14:39:25 +0900

    testHTTP

---
+++ server.ts
@@ -0,0 +1,20 @@
+import express, { Application, Request, Response } from 'express';
+import { load } from 'ts-dotenv';
+
+const app: Application = express();
+const env = load({
+  PORT: Number
+})
+app.get('/', async (_: Request, res: Response): Promise<Response> => {
+  return res.status(200).send({
+    message: 'Hello World!'
+  });
+});
+app.get('/message', async (_: Request, res: Response): Promise<Response> => {
+  return res.status(200).send({
+    message: 'HTTP?'
+  });
+});
+
+app.listen(env.PORT, () => {
+});

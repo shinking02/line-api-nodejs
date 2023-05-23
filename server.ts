2ce722f9c36ec940155520b5f0699c32e7bfd0f4
shinking02 <k.shin0102@gmail.com>
2023-05-23 11:57:36 +0900

    tesDotEnv

--- server.ts
+++ server.ts
@@ -1,12 +1,15 @@
 import express, { Application, Request, Response } from 'express';
-
+import { load } from 'ts-dotenv';
 const PORT = 8080;
 
 const app: Application = express();
-
+const a = load({
+    BUCKET_NAME: String,
+});
 app.get('/', async (_: Request, res: Response): Promise<Response> => {
   return res.status(200).send({
     message: 'Hello World!',
+    BUCKET_NAME: a.BUCKET_NAME
   });
 });
 
